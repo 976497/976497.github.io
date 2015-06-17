44d2601becd3c022669e8aa2bd1345fa257d4bc7
976497 <976497@gmail.com>
2015-06-17 19:57:47 +0100

    website 3 commit

---
+++ work.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!-- Copyright Ⓒ 2015 976497 <http://976497.byethost/contact/> (976497[at]gmail.com) - GNU General Public License (GPLv3). This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. This notice must stay intact.-->
+<html lang="en-EN">
+ <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="robots" content="index,follow,all">
+  <meta http-equiv="content-language" content="en">
+  <meta name="description" content="PROGRAMMING SUBCONTRACTOR">
+  <meta name="revisit-after" content="7 days">
+  <meta name="keywords" content="programming subcontractor">
+  <link rel="shortcut icon" href="./xmain/favicon.ico">
+  <link rel="stylesheet" type="text/css" href="./xmain/xindex.css" />
+  <link rel="stylesheet" type="text/css" href="./xtox/xbtm.css" />
+  <link rel="stylesheet" type="text/css" href="./xtoy/xtop.css" />
+  <link rel="stylesheet" type="text/css" href="./xprg/xprg.css" />
+  <title>976497[at]gmail.com - BUSY OR NOT</title>
+ </head>
+
+ <body style="font-family:'Lucida Console', Monaco, monospace">
+
+ <?php $ktobar='WORK';include('./xtoy/xtop.php');?>
+ <h1>FREE TO WORK</h1>
+ <?php include('./xtox/xbtm.php');?>
+ </body>
+</html>
---
+++ xtox/xbtm.php
@@ -0,0 +1,16 @@
+<br /></br />
+<div class="DCbtmA">
+ <div class="DCbtmB">
+
+<div class="DCadvX">
+<!-- ADS -->
+</div>
+
+  <div class="DCbtmC">
+   <table class="TCbtmA">
+    <tr><td>LAST UPDATE: </td><td>2015.06.16</td></tr>
+    <tr><td>STARTED: </td><td>2015.06.16</td></tr>
+   </table>
+  </div>
+ </div>
+</div>
